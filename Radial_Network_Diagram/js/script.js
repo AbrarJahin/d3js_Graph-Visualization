dd19f936281ed13f4c12bb8c61db165738f30c8e
Abrar Jahin <abrarjahin@live.com>
2022-03-13 23:54:46 -0400

    Radial Network Diagram backup added

--- Radial_Network_Diagram/js/script.js
+++ Radial_Network_Diagram/js/script.js
@@ -1,8 +1,8 @@
-import { drawForcedDirectedGraph as ForcedDirectedGraph } from './ForcedDirectedGraph/forcedDirectedGraph.js';
+import { drawRadialNetworkDiagram as RadialNetworkDiagram } from './RadialNetworkDiagram/drawRadialNetworkDiagram.js';
 
 (function() { //OnLoad event
-  d3.json("./data/graph3.json", function (error, graph) {
-    if (error) throw error;
-    ForcedDirectedGraph(graph, "#force_directed_graph");
-  });
+	d3.json("./data/adjacency_list.json", function(error, classes) {
+		if (error) throw error;
+		RadialNetworkDiagram(classes);
+	});
 })();
